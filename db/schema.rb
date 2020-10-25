6a231fa782265d3cf55522746b4b92bcecbd413e
nozomi <kkk@docomo.ne.jp>
2020-10-25 21:49:13 +0900

    Houseモデルuser_idカラム削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_25_120638) do
+ActiveRecord::Schema.define(version: 2020_10_25_124633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 2020_10_25_120638) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
