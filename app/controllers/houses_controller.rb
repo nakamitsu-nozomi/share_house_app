519081a70cdd5030388b0df440bb9d5d78ebc5f6
nozomi <kkk@docomo.ne.jp>
2020-10-25 12:14:03 +0900

    物件新規投稿機能実装（画像以外）

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -7,9 +7,12 @@ class HousesController < ApplicationController
   end
 
   def new
+    @house= House.new
   end
 
   def create
+    house =House.create!(house_params)
+    redirect_to house
   end
 
   def edit
@@ -19,5 +22,10 @@ class HousesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def house_params
+    params.require(:house).permmit(:name,:house_image,:house_rent,:service_fee,:station,:access,:house_size,:convenience,:content)
   end
 end
