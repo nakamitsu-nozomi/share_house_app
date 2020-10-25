14a8c06754adcd167e6a48cd97a71f903a411d42
nozomi <kkk@docomo.ne.jp>
2020-10-25 14:53:09 +0900

    CloudFrontの導入

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -7,5 +7,6 @@ if Rails.env.production?
       region: ENV['S3_REGION']
     }
     config.fog_directory = ENV['S3_BUCKET']
+    config.asset_host ="https://d2w3q170ysj3d4.cloudfront.net"
   end
 end
