7a55b453b19d177dc0492ec5932ccc01d4785bfb
nozomi <kkk@docomo.ne.jp>
2020-10-25 18:37:33 +0900

    ユーザー登録、編集項目を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  mount_uploader :user_icon, ImageUploader
+  validates :experience, inclusion: { in: [true, false] }
+  
 end
