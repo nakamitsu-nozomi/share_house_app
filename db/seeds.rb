d7683e9a126341928fad3eda91301ab802196169
nozomi <kkk@docomo.ne.jp>
2020-10-25 18:57:27 +0900

    Userのseedデータを作成

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+EMAIL = 'test@example.com'
+PASSWORD = 'password'
+NAME = "test"
+USER_ICON = "public/images/fallback/default.png"
+ADRESS = "東京"
+EXPERIENCE = true
+# テストユーザーが存在しないときだけ作成
+User.find_or_create_by!(email: EMAIL) do |user|
+  user.password = PASSWORD
+  user.name = NAME
+  user.user_icon =USER_ICON
+  user.adress = ADRESS
+  user.experience = EXPERIENCE
+  puts 'ユーザーの初期データインポートに成功しました。'
+end 
