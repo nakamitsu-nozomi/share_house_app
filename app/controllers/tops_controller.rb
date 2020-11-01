c5cb74ead9710e60baecf9b1b7f666ff57c1ce71
nozomi <kkk@docomo.ne.jp>
2020-11-01 12:54:58 +0900

    topコントローラーとビューを作成

---
+++ app/controllers/tops_controller.rb
@@ -0,0 +1,7 @@
+class TopsController < ApplicationController
+  def index
+  @q = House.ransack(params[:q])
+    @houses= @q.result(distinct: true)
+    @areas=Area.all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "houses#index"
+  root "tops#index"
   resources :houses
   resource :users, only: :show
   resource :areas
