8753e9376be105b1376be5ff5130130523b4d5af
nozomi <kkk@docomo.ne.jp>
2020-11-03 11:24:13 +0900

    保存ボタンを非同期化

--- app/controllers/clips_controller.rb
+++ app/controllers/clips_controller.rb
@@ -1,11 +1,11 @@
 class ClipsController < ApplicationController
   def create
-    current_user.clips.create!(house_id: params[:house_id])
-    redirect_back(fallback_location: root_path)
+    @house= House.find(params[:house_id])
+    current_user.clips.create!(house_id: @house.id)
   end
 
   def destroy
-    current_user.clips.find_by(house_id: params[:house_id]).destroy!
-    redirect_back(fallback_location: root_path)
+    @house= House.find(params[:house_id])
+    current_user.clips.find_by(house_id: @house.id).destroy!
   end
 end
