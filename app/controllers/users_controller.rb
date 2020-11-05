a65138c816475ca57fe282edd7a9abe22a519d52
nozomi <kkk@docomo.ne.jp>
2020-11-05 10:28:49 +0900

    ヘッダーにリンク追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
     @user=User.find(current_user.id)
     @houses = House.where(user_id: current_user.id)
+    @clips= @user.cliped_houses
   end
   def clips
     @user=User.find(current_user.id)
