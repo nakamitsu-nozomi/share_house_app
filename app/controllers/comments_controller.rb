16dc08c4bec6e93eb537184acaa2a03b50775e63
nozomi <kkk@docomo.ne.jp>
2020-11-06 11:04:40 +0900

    口コミの新規投稿、一覧表示機能実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,54 @@
 class CommentsController < ApplicationController
+  before_action :set_house, only: [:index, :new, :create, :edit]
+  def index
+    @comments=Comment.where(house_id:@house.id )
+  end
+
+  def show
+  end
+
+  def new
+    @comment=Comment.new
+  end
+
+  def create
+    @comment=Comment.create!(
+      title: comment_params[:title],content: comment_params[:content],user_id: current_user.id,house_id: @house.id,type: comment_params[:type],star: comment_params[:star],
+      clean_review: comment_params[:clean_review],houserule_review: comment_params[:houserule_review],location_review: comment_params[:location_review],
+      com_review: comment_params[:com_review]
+    )
+    if @comment.save
+      redirect_to house_comments_path(house_id:@house.id),notice: "物件を登録しました"
+    else
+      flash.now[:alert] ="投稿に失敗しました"
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def delete
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+
+  private
+  def comment_params
+    params.require(:comment).permit(
+      :content,:user_id,:house_id,:type,:star, :title,:clean_review,
+      :houserule_review,:location_review,:com_review
+    )
+  end
+
+  def set_house
+    @house = House.find(params[:house_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root "tops#index"
   resources :houses do
     resource :clips,only: [:create,:destroy]
+    resources :comments
   end
   resource :users, only: [:show] do
     collection do
