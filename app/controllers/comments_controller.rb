14d19963d872eed54c3310cbf8f99ddbb05d7af1
nozomi <kkk@docomo.ne.jp>
2020-11-06 22:00:14 +0900

    口コミ編集機能実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,8 @@
 class CommentsController < ApplicationController
-  before_action :set_house, only: [:index, :new, :create, :edit]
+  before_action :set_house, only: [:index, :new, :create, :edit,:update]
   def index
     @comments=Comment.where(house_id:@house.id )
+    @comment = Comment.new
   end
 
   def show
@@ -12,7 +13,7 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @comment=Comment.create!(
+    @comment=Comment.create(
       title: comment_params[:title],content: comment_params[:content],user_id: current_user.id,house_id: @house.id,type: comment_params[:type],star: comment_params[:star],
       clean_review: comment_params[:clean_review],houserule_review: comment_params[:houserule_review],location_review: comment_params[:location_review],
       com_review: comment_params[:com_review]
@@ -26,13 +27,20 @@ class CommentsController < ApplicationController
   end
 
   def edit
-
+ 
   end
 
   def delete
   end
 
   def update
+    @comment.update(comment_params)
+    if @comment.update(comment_params)
+      redirect_to house_comments_path(house_id:@house.id),notice: "口コミを編集しました"
+    else
+      flash.now[:alert] ="編集に失敗しました"
+      render :edit
+    end
   end
 
   def destroy
@@ -50,5 +58,6 @@ class CommentsController < ApplicationController
 
   def set_house
     @house = House.find(params[:house_id])
+    @comment =Comment.find_by(house_id: params[:house_id])
   end
 end
