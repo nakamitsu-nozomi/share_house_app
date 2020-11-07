f6e87dc90dee06d2c094ee116dfb08e4499c8d9c
nozomi <kkk@docomo.ne.jp>
2020-11-07 09:33:46 +0900

    エラー文を表示

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,9 +6,8 @@ class Comment < ApplicationRecord
   validates :type, inclusion: { in: [true, false] }
   validates :content, presence: true
   validates :star, presence: true,numericality: { only_integer: true,greater_than: 0, less_than: 6 }
-  validates :clean_review, presence: true,numericality: { only_integer: true,greater_than: 1, less_than: 6 }
-  validates :houserule_review, presence: true,numericality: { only_integer: true,greater_than: 1, less_than: 6 }
-  validates :location_review, presence: true,numericality: { only_integer: true,greater_than: 1, less_than: 6 }
-  validates :com_review, presence: true,numericality: { only_integer: true,greater_than: 1, less_than: 6 }
-
+  validates :clean_review, presence: true,numericality: { only_integer: true,greater_than: 0, less_than: 6 }
+  validates :houserule_review, presence: true,numericality: { only_integer: true,greater_than: 0, less_than: 6 }
+  validates :location_review, presence: true,numericality: { only_integer: true,greater_than: 0, less_than: 6 }
+  validates :com_review, presence: true,numericality: { only_integer: true,greater_than: 0, less_than: 6 }
 end
