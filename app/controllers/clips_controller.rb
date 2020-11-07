10a6d91f055e5f242009d9f58f8c4faafcfa488f
nozomi <kkk@docomo.ne.jp>
2020-11-07 10:37:53 +0900

    マイページにレビュー物件一覧表示

--- app/controllers/clips_controller.rb
+++ app/controllers/clips_controller.rb
@@ -3,11 +3,13 @@ class ClipsController < ApplicationController
     current_user.clips.create!(house_id: params[:house_id])
     @house= House.find(params[:house_id])
     @myhouse=House.where(user_id: current_user.id)
+    @comments= Comment.where(user_id: current_user.id)
   end
 
   def destroy
     current_user.clips.find_by(house_id: params[:house_id]).destroy!
     @house= House.find(params[:house_id])
     @myhouse=House.where(user_id: current_user.id)
+    @comments= Comment.where(user_id: current_user.id)
   end
 end
