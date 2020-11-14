b7938867216385c82cad284cd4fccf268076eb94
nozomi <kkk@docomo.ne.jp>
2020-11-14 13:20:49 +0900

    ゲストユーザーログイン機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,26 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers:{
+    registrations: "users/registrations"
+  }
+
   root "tops#index"
+
   resources :houses do
     resource :clips,only: [:create,:destroy]
     resources :comments do
       resource :clips,only: [:create,:destroy]
     end
   end
+
   resource :users, only: [:show] do
     collection do
       get :clips
       get :comments
     end
   end
+
   resource :areas
+  devise_scope :user do
+     post 'users/guest_sign_in', to: 'users/sessions#new_guest' 
+  end
 end
