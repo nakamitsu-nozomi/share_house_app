df0d62c5ba4f35312ed9ad7e2fed54a10967b1a1
nozomi <kkk@docomo.ne.jp>
2020-11-14 17:55:27 +0900

    googleマップ表示実装

---
+++ config/initializers/geocoder.rb
@@ -0,0 +1,16 @@
+
+Geocoder.configure(
+
+    # street address geocoding service (default :nominatim)
+    lookup: :google,
+
+    # to use an API key:
+    api_key: "AIzaSyCviXl7JqAtAgCmgeMfZYKb0FZYqaONLwc",
+
+    # geocoding service request timeout, in seconds (default 3):
+    timeout: 5,
+
+    # set default units to kilometers:
+    units: :km
+
+)
