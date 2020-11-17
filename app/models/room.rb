ee11b4bd5494e371b0c8e2541ba859c72e28e945
nozomi <kkk@docomo.ne.jp>
2020-11-17 09:32:02 +0900

    Roomモデルにバリテーション追加

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,8 @@
 class Room < ApplicationRecord
   belongs_to :house
+  validates :size, presence: true,numericality: { only_integer: true,greater_than: -1}
+  validates :rent, presence: true,numericality: { only_integer: true,greater_than: -1}
+  validates :room_type, presence: true
+  validates :vacancy, presence: true
+
 end
