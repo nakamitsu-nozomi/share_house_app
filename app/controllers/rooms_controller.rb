109d24dba0545d734ec177e457162b9e86e9e552
nozomi <kkk@docomo.ne.jp>
2020-11-20 09:27:59 +0900

    部屋情報の削除機能実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -22,15 +22,32 @@ class RoomsController < ApplicationController
   end
   
   def edit
+    @room = Room.find_by(house_id:params[:house_id],id:params[:id])
   end
 
   def update
+    @house=House.find(params[:house_id])
+    @room= Room.find_by(house_id: params[:house_id],id: params[:id])
+    @room.house_id=@house.id
+    if @room.update(update_room_params)
+      redirect_to room_house_path(@house),notice: "更新しました"
+    else
+      flash.now[:alert] ="更新に失敗しました"
+      render :edit    
+    end
   end
 
   def destroy
+    @house=House.find(params[:house_id])
+    @room= Room.find_by(house_id: params[:house_id],id: params[:id])
+    @room.destroy!
+    redirect_to room_house_path(@house),alert: "削除しました"
   end
   private
   def room_params
     params.permit(:room_num,:vacancy,:image,:size,:rent,:room_type)
+  end
+  def room_params
+    params.permit(:room_num,:vacancy,:image,:size,:rent,:room_type,:_destroy, :id)
   end
 end
