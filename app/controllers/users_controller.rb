41810145844bcf6d99c9a70c086c6313487bfcab
nozomi <kkk@docomo.ne.jp>
2020-11-21 18:01:45 +0900

    マイページリンクとレイアウト修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,16 +8,17 @@ class UsersController < ApplicationController
   end
   def clips
     @user=User.find(current_user.id)
-    @houses= current_user.cliped_houses
-    @myhouse=House.where(user_id: current_user.id)
+    @houses = House.where(user_id: current_user.id)
     @comments= Comment.where(user_id: current_user.id)
+    @clips= @user.cliped_houses
+
   end
 
   def comments
-    @user = User.find(current_user.id)
-    @houses = current_user.comments_houses
-    @myhouse = House.where(user_id: current_user.id)
-    @cliphouse = current_user.cliped_houses
+    @user=User.find(current_user.id)
+    @houses = House.where(user_id: current_user.id)
+    @comments= current_user.comments_houses
+    @clips= @user.cliped_houses
   end
 
 
