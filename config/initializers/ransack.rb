471ff9fa1fad6e12b4a22c5a6a3efacc92f7b3ce
nozomi <kkk@docomo.ne.jp>
2020-11-24 21:53:27 +0900

    ソート実装 ブラウザバック時の星のバグ解消

---
+++ config/initializers/ransack.rb
@@ -0,0 +1,12 @@
+
+
+Ransack.configure do |config|
+  
+  config.custom_arrows = {
+  # 上向きの矢印
+  # HTMLで指定可能
+  up_arrow: '<i class="fas fa-arrow-circle-up"></i>',
+  # 下向きの矢印
+  down_arrow: '<i class="fas fa-arrow-circle-down"></i>'
+}
+end
